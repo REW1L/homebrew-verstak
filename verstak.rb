e973a5f10b207a69e5136269f06f63a20a46bf09
Ivan Danilov <ivandani@yandex.ru>
2020-09-25 22:38:17 +0300

    Init

---
+++ verstak.rb
@@ -0,0 +1,24 @@
+class Verstak < Formula
+  include Language::Python::Virtualenv
+  homepage "https://github.com/REW1L/verstak"
+  url "https://github.com/REW1L/verstak.git",
+    :using => :git,
+    :tag => "0.0.2",
+    :revision => "3226d1e5085c35fa5f1c7b46b35af8c1d7a51d5d"
+
+  depends_on "python@3"
+
+  resource "lxml" do
+    url "https://files.pythonhosted.org/packages/2c/4d/3ec1ea8512a7fbf57f02dee3035e2cce2d63d0e9c0ab8e4e376e01452597/lxml-4.5.2.tar.gz"
+    sha256 "cdc13a1682b2a6241080745b1953719e7fe0850b40a5c71ca574f090a1391df6"
+  end
+
+  resource "python-docx" do
+    url "https://files.pythonhosted.org/packages/e4/83/c66a1934ed5ed8ab1dbb9931f1779079f8bca0f6bbc5793c06c4b5e7d671/python-docx-0.8.10.tar.gz"
+    sha256 "bc76ecac6b2d00ce6442a69d03a6f35c71cd72293cd8405a7472dfe317920024"
+  end
+
+  def install
+    virtualenv_install_with_resources :using => "python@3"
+  end
+end
